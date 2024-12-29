0fe9e72dc9f010b63c00a91204756d9c703de3ff
DzarZaki <dzarzaki2@gmail.com>
2024-12-29 15:40:00 +0700

    Menambahkan InsertViewModel untuk mengelola logika penambahan data mahasiswa dengan dukungan pembaruan UI state dan penyimpanan data melalui MahasiswaRepository

---
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.pertemuan12.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan12.repository.MahasiswaRepository
+
+class InsertViewModel (private val mhs: MahasiswaRepository) : ViewModel() {
+    var uiState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent) {
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertMhs() {
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
