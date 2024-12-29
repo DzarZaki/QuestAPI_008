902eb9f341a3076a55b074125fb69b265ddc8b52
DzarZaki <dzarzaki2@gmail.com>
2024-12-29 16:04:43 +0700

    Menambahkan navigasi balik opsional pada CenterAlignedTopAppBar menggunakan ikon panah kembali dan mendukung modifier serta scrollBehavior

--- app/src/main/java/com/example/pertemuan12/ui/costumwidget/CostumeTopAppBar.kt
+++ app/src/main/java/com/example/pertemuan12/ui/costumwidget/CostumeTopAppBar.kt
@@ -1,14 +1,17 @@
 package com.example.pertemuan12.ui.costumwidget
 
+import androidx.compose.foundation.clickable
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import java.lang.reflect.Modifier
+import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,4 +34,17 @@ fun CostumeTopAppBar(
                 }
             )
         },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+            }
+        }
+    )
 }
