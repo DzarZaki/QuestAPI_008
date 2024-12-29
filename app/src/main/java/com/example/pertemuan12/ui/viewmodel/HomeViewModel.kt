21fc7f53e6a92b7d014e8ee45f49d1c48569a5c3
DzarZaki <dzarzaki2@gmail.com>
2024-12-29 15:50:07 +0700

    Menambahkan HomeViewModel untuk mengelola data mahasiswa dengan status UI Loading, Success, dan Error, serta fungsi getMhs untuk pengambilan data dari repository

--- app/src/main/java/com/example/pertemuan12/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/HomeViewModel.kt
@@ -1,6 +1,13 @@
 package com.example.pertemuan12.ui.viewmodel
 
+import android.net.http.HttpException
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan12.model.Mahasiswa
+import com.example.pertemuan12.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 sealed class HomeUiState {
     data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
@@ -8,5 +15,24 @@ sealed class HomeUiState {
     object Loading : HomeUiState()
 }
 
-class HomeViewModel {
+class HomeViewModel (private val mhs: MahasiswaRepository) : ViewModel() {
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs() {
+        viewModelScope.launch {
+            mhsUiState = HomeUiState.Loading
+            mhsUiState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
 }
