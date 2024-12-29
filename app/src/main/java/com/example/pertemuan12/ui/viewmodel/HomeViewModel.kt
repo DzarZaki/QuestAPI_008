c4d9a111b0cc0594877b525c678af63a6ecf2e42
DzarZaki <dzarzaki2@gmail.com>
2024-12-29 17:22:21 +0700

    Memperbaiki code

--- app/src/main/java/com/example/pertemuan12/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewmodel/HomeViewModel.kt
@@ -1,21 +1,23 @@
 package com.example.pertemuan12.ui.viewmodel
 
-import android.net.http.HttpException
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.example.pertemuan12.model.Mahasiswa
 import com.example.pertemuan12.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 import java.io.IOException
 
-sealed class HomeUiState {
-    data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
-    object Error : HomeUiState()
-    object Loading : HomeUiState()
+sealed class HomeUiState{
+    data class Success(val mahasiswa: List<Mahasiswa>):HomeUiState()
+    object Error:HomeUiState()
+    object Loading:HomeUiState()
 }
 
-class HomeViewModel (private val mhs: MahasiswaRepository) : ViewModel() {
+class HomeViewModel(private val mhs: MahasiswaRepository):ViewModel() {
     var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
         private set
 
@@ -35,13 +37,14 @@ class HomeViewModel (private val mhs: MahasiswaRepository) : ViewModel() {
             }
         }
     }
-    fun deleteMhs(nim: String) {
+
+    fun deleteMhs(nim: String){
         viewModelScope.launch {
             try {
                 mhs.deleteMahasiswa(nim)
-            } catch (e: IOException) {
+            }catch (e: IOException){
                 HomeUiState.Error
-            } catch (e: HttpException) {
+            }catch (e: HttpException){
                 HomeUiState.Error
             }
         }
