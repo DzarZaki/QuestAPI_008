c4db311accf4a5d65a7607393295ec51648a6502
DzarZaki <dzarzaki2@gmail.com>
2024-12-29 16:15:19 +0700

    Menambahkan komponen EntryBody ke EntryMhsScreen dengan dukungan pengguliran vertikal, pembaruan state input, dan penyimpanan data mahasiswa melalui InsertViewModel

--- app/src/main/java/com/example/pertemuan12/ui/view/InsertView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/view/InsertView.kt
@@ -1,5 +1,9 @@
 package com.example.pertemuan12.ui.view
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
@@ -11,6 +15,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan12.ui.costumwidget.CostumeTopAppBar
 import com.example.pertemuan12.ui.navigation.DestinasiNavigasi
 import com.example.pertemuan12.ui.viewmodel.InsertViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
@@ -37,6 +42,20 @@ fun EntryMhsScreen(
                 navigateUp = navigateBack
             )
         }
-    ){
-
+    ){ innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onSiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertMhs()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
     }
+}
